81949a99069da1bcff49d90476c22a5338bddfad
Ilya Siganov <ilya.blan4@gmail.com>
2023-12-08 22:37:55 +0600

    add day_2-2

--- day_2.c
+++ day_2.c
@@ -53,7 +53,7 @@ int parse(const char* line, Stats constraint) {
   return game_idx;
 }
 
-int main(void) {
+void task1() {
   char line[255];
   Stats constraint = {.red=12, .green=13, .blue=14};
   
@@ -63,4 +63,58 @@ int main(void) {
   }
 
   printf("Result = %d\n", res);
+}
+
+int parse2(char* line) {
+  line = strchr(line, ':'); // find :
+  line += 2; // skip : and space
+
+  Stats stats= {.red=0, .green=0, .blue=0};
+
+  while(line != NULL && line[0] != '\0') {
+    int v;
+    sscanf(line, "%d", &v);
+    printf("%d=", v);
+    line = strchr(line, ' '); // find space
+    line++; // skip space
+    sscanf(line, "%s", buf);
+    printf("%s ", buf);
+    switch (buf[0]) {
+    case 'r':
+      if (v > stats.red) stats.red = v;
+      break;
+    case 'g':
+      if (v > stats.green) stats.green = v;
+      break;
+    case 'b':
+      if (v > stats.blue) stats.blue = v;
+      break;
+    default:
+      printf("BAD TOKEN: '%s'\n", buf);
+      break;
+    }
+
+    line += strlen(buf);
+    if (line[0] == ' ') line++;
+    if (line[0] == '\n' || line[0] == '\0') break;
+  }
+  printf("\n");  
+
+  return stats.red * stats.green * stats.blue;
+}
+
+void task2() {
+  char line[255];
+
+  int res = 0;
+  while(fgets(line, sizeof line, stdin) != NULL) {
+    res += parse2(line);
+  }
+
+  printf("Result = %d\n", res);
+}
+
+int main(void) {
+  // task1();
+  task2();
 }
